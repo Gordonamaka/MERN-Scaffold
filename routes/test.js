192c342a9475ebf62b543b8572e17bcb7b45745f
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-03-06 15:50:44 -0500

    Implementing env variables

---
+++ routes/test.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+// Get all test info
+router.get()
+// Getting a single test
+
+// Creating a test
+
+// Updating a test
+
+// Deleting a test
+
+
+
+module.exports = router
--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
+require('dotenv').config();
 const express = require("express");
-
-
+const mongoose = require('mongoose');
 const app = express();
-const port = 3001
 
+// This is just for the connection for the test Database
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+
+// Port from Env & to allow our server to accept JSON data
+const port = process.env.SERVER_PORT
+app.use(express.json())
 
 app.get("/", (_, res) => res.send("Hello World"));
+
+
+// Routes
+const testRouter = require('../routes/test');
+app.use('/test', testRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port:${port}`)
